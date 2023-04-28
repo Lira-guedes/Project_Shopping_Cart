a3948b6887842f3937b7317ba4e704707ce13bf9
Lira Guedes <liracrg@gmail.com>
2023-04-28 11:04:04 -0300

    Requisitos 8 e 9

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,22 @@
-export const getAddress = () => {
-  // seu código aqui
+const cartAdress = document.querySelector('.cart__address');
+const input = document.querySelector('.cep-input');
+
+export const getAddress = async (CEP) => {
+  const apis = [
+    (await fetch(`<https://cep.awesomeapi.com.br/json/${CEP}>`)).json,
+    (await fetch(`https://brasilapi.com.br/api/cep/v2/${CEP}>`)).json,
+  ];
+  const data = await Promise.any(apis);
+  return data;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cep = getAddress(input.value);
+  if (cep.distric) {
+    cartAdress.innerHTML = `${cep.address}-${cep.district}-${cep.city}-${cep.state}`;
+  } else if (cep.neighborhood) {
+    cartAdress.innerHTML = `${cep.street}-${cep.neighborhood}-${cep.city}-${cep.state}`;
+  } else {
+    cartAdress.innerHTML = 'CEP não encontrado';
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
+import { saveCartID, getSavedCartIDs } from './helpers/cartFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
@@ -10,6 +11,10 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 // ## 5. Exiba uma mensagem de erro caso algo dê errado na requisição à API
 
 const products = document.querySelector('.products');
+const cart = document.querySelector('.cart__products');
+const savedCartIds = getSavedCartIDs();
+const buttonCep = document.querySelector('.cep-button');
+
 // cria carregamento
 const startLoading = () => {
   const p = document.createElement('p');
@@ -29,6 +34,23 @@ const createError = () => {
   p.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
   products.appendChild(p);
 };
+// btn que adiciona ao carrinho
+const cartButtons = () => {
+  const itemBtn = document.querySelectorAll('.product__add');
+  itemBtn.forEach((button) => button.addEventListener('click', async (elem) => {
+    const id = elem.target.parentNode.firstChild.innerText;
+    const data = await fetchProduct(id);
+    const product = createCartProductElement(data);
+    cart.appendChild(product);
+    saveCartID(id);
+  }));
+};
+// salva carrinho no local storage
+async function saveCart() {
+  const items = await Promise.all(savedCartIds.map(fetchProduct));
+  const elementProduct = items.map(createCartProductElement);
+  elementProduct.forEach((product) => cart.append(product));
+}
 
 async function loadProducts() {
   startLoading();
@@ -42,6 +64,11 @@ async function loadProducts() {
     createError();
   } finally {
     stopLoading();
+    cartButtons();
   }
 }
+saveCart();
 loadProducts();
+
+// busca cep
+buttonCep.addEventListener('click', searchCep);
